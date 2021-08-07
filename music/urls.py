8b70bdc64d9972c526cbcd0947665d0aeb32d968
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-07 18:48:16 -0500

    complete get, put, post, delete in postman. all functiong as required.

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-# import views so we can register our class-based views in path below
+
 from . import views
 
 urlpatterns = [
-    # add new path with music/ as the endpoint
-    # register our model class list class based view as a view (query)
     path('music/', views.SongList.as_view()),
+    path('music/<int:pk>/', views.SongDetail.as_view()),
+    path('music/<int:pk>/edit/', views.SongDetail.as_view()),
+    path('music/<int:pk>/delete/', views.SongDetail.as_view()),
 ]
--- music/views.py
+++ music/views.py
@@ -1,45 +1,58 @@
+from django.http import response
+from django.http.response import HttpResponse
 from django.shortcuts import render
-# imports Song model and songserializer - used for endpoint class-based view methods
+
 from .models import Song
 from .serializers import SongSerializer
-# parent class for our class-based views - takes care of nuts and bolts of method routing/response to requestor
 from rest_framework.views import APIView
-# function call that converts our data into JSON string literal, and sends it to client requestion resources at specified endpoint
 from rest_framework.response import Response
-# enum (collection of values) the specify what status cude we would like to send back in response
 from rest_framework import status
 
 
-# Create your views here. - these handle requests made to our API
+# Create your views here.
 class SongList(APIView):
-
-    # take in request parameter - which contains info about client's request
-    # In APIs, requests can contain a lot of useful info (i.e. - parameters, headers, authentication tokens, and request type)
     def get(self, request):
-        # query all the rows from our model table/db
         song = Song.objects.all()
-
-        # pass the song query above into a new instantiation of our Serializer
-        # many=True tells serializer that it is receiving a query set (object of many rows)
-        # serializer takes all objects and convert them into Python native data type - in this case a list of dictionaries
         serializer = SongSerializer(song, many=True)
 
-        # pass serializer.data (data property contains all model objects in a list of dictionaries) into Response object we are returning
-        # response object converts list of dictionaries into a JSON string literal and returns it to requestor
         return Response(serializer.data)
 
-    # endpoint allowing us to POST or create a new row in our database
     def post(self, request):
-        # take data from body of request.data and pass into new instantiation of the serializer
         serializer = SongSerializer(data=request.data)
 
-        # use is_valid() method to confirm the request's data contains all required fields present in our model definition
         if serializer.is_valid():
-            # if True, JSON data from request's body will be converted into new model instance and saved to our table in db
             serializer.save()
 
-            # returns serializer.data  and contet created message
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        # return this if serializer is not valid
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+    def get_by_id(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            return HttpResponse(status=404)
+        
+    def get(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song)
+
+        return Response(serializer.data)
+        
+    def put(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+        
+    def delete(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song)
+
+        song.delete()
+        
+        return Response(serializer.data, status=200)
